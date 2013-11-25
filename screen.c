2f3ec10586f5a5d307d530c1cb3d34693b2bfe01
David Francis <dfran1991@gmail.com>
2013-11-25 14:29:30 -0500

    Refactored code. Moved screen related code to own files.

---
+++ screen.c
@@ -0,0 +1,113 @@
+#include "screen.h"
+
+
+uint8_t make_color(enum vga_color fg, enum vga_color bg)
+{
+	return fg | bg << 4;
+}
+
+uint16_t make_vgaentry(char c, uint8_t color)
+{
+	uint16_t c16 = c;
+	uint16_t color16 = color;
+	return c16 | color16 << 8;
+}
+
+size_t strlen(const char* str)
+{
+	size_t ret = 0;
+	while ( str[ret] != 0 )
+		ret++;
+	return ret;
+}
+
+static void scroll()
+{
+	uint8_t attributeByte = ( COLOR_BLACK << 4 ) | ( COLOR_WHITE & 0x0F );
+	uint16_t blank = 0x20 | (attributeByte << 8);
+	
+	if (terminal_column > VGA_HEIGHT)
+	{
+		int i;
+		for(i = 0; i < VGA_WIDTH*VGA_HEIGHT; i++)
+		{
+			terminal_buffer[i] = terminal_buffer[i+VGA_HEIGHT];
+		}
+		
+		for(i = VGA_WIDTH*VGA_HEIGHT; i < (VGA_WIDTH+1)*VGA_HEIGHT; i++)
+		{
+			terminal_buffer[i] = blank;
+		}
+		
+		terminal_row = VGA_HEIGHT;
+	}
+}
+
+static void move_cursor()
+{
+	uint16_t cursorLocation = terminal_row * VGA_WIDTH + terminal_column;
+	outb(0x3D4, 14);
+	outb(0x3D5, cursorLocation >> 8);
+	outb(0x3D4, 15);
+	outb(0x3D5, cursorLocation);
+}
+
+void terminal_initialize()
+{
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	for ( size_t y = 0; y < VGA_HEIGHT; y++ )
+	{
+		for ( size_t x = 0; x < VGA_WIDTH; x++ )
+		{
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+		}
+	}
+}
+
+void terminal_setcolor(uint8_t color)
+{
+	terminal_color = color;
+}
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
+{
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = make_vgaentry(c, color);
+}	
+
+void terminal_putchar(char c)
+{
+	if ( c == '\n' )
+	{
+		terminal_column = 0;
+		if ( ++terminal_row == VGA_HEIGHT )
+		{
+			terminal_row = 0;
+		}
+	}
+	else
+	{	
+		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		if ( ++terminal_column == VGA_WIDTH )
+		{
+			terminal_column = 0;
+			if ( ++terminal_row == VGA_HEIGHT )
+			{
+				terminal_row = 0;
+			}	
+		}
+	}
+
+	move_cursor();
+}
+
+void terminal_writestring(const char* data)
+{		
+	size_t datalen = strlen(data);
+	for ( size_t i = 0; i < datalen; i++)
+		terminal_putchar(data[i]);
+}	
