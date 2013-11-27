8e090efec0771b3608c9709aa73bb70c08e6704f
David Francis <dfran1991@gmail.com>
2013-11-26 22:56:42 -0500

    More type errors

--- kheap.c
+++ kheap.c
@@ -24,17 +24,17 @@ uint32_t kmalloc_a(uint32_t sz)
 	return kmalloc_int(sz, 1, 0);
 }
 
-u32int kmalloc_p(u32int sz, u32int *phys)
+uint32_t kmalloc_p(uint32_t sz, uint32_t *phys)
 {
     return kmalloc_int(sz, 0, phys);
 }
 
-u32int kmalloc_ap(u32int sz, u32int *phys)
+uint32_t kmalloc_ap(uint32_t sz, uint32_t *phys)
 {
     return kmalloc_int(sz, 1, phys);
 }
 
-u32int kmalloc(u32int sz)
+uint32_t kmalloc(uint32_t sz)
 {
     return kmalloc_int(sz, 0, 0);
 }
